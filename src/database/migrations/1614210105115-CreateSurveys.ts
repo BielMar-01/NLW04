9f88a0e519f4d60b99e8233fd877dc542bc073c1
Gabriel <bielmarques922@gmail.com>
2021-02-25 21:24:14 -0300

    Aula 03 - Finalizada

---
+++ src/database/migrations/1614210105115-CreateSurveys.ts
@@ -0,0 +1,38 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { QueryExpressionMap } from "typeorm/query-builder/QueryExpressionMap";
+
+export class CreateSurveys1614210105115 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "surveys",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "title",
+                        type: "varchar"
+                    },
+                    {
+                        name: "description",
+                        type: "varchar"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("surveys");
+    }
+
+}
